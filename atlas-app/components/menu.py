b8a66d998fe08bd35eccb1b6f9fc58c17b441758
ldnelson16 <ldnelson16@gmail.com>
2023-10-18 22:16:13 -0400

    Abstracting menu

--- atlas-app/components/menu.py
+++ atlas-app/components/menu.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 from commands.change_page import change_page
+import json
+
+with open("atlas-app/pages.json", 'r') as json_file:
+  paths = json.load(json_file)
 
 def on_menu_hover(event, item_name):
     # Display the dropdown menu item when the mouse enters the menu item
@@ -13,15 +17,15 @@ def menu(root):
   # Create a Menu widget for the top-level items
   menu = tk.Menu(root)
 
-  # Create a Menu widget for the dropdown
-  dropdown_menu1 = tk.Menu(menu, tearoff=0)
-
-  # Add items to the dropdown menu
-  dropdown_menu1.add_command(label="Total Evaluation",command=lambda:change_page("Total Evaluation",root))
-  dropdown_menu1.add_command(label="Workload Evaluation",command=lambda:change_page("Workload Evaluation",root))
-
-  # Add top-level items with dropdown menus
-  menu.add_cascade(label="Semester Analysis", menu=dropdown_menu1)
+  for key,value in paths.items():
+    if isinstance(value,dict):
+      # Create a Menu widget for the dropdown
+      dropdown_menu = tk.Menu(menu, tearoff=0)
+      # Add items to the dropdown menu
+      for item,val in value.items():
+        dropdown_menu.add_command(label=val,command=lambda:change_page(val,root))
+      # Add top-level items with dropdown menus
+      menu.add_cascade(label=key.replace("_"," ").title(), menu=dropdown_menu)
 
   # Attach the menu to the root window
   root.config(menu=menu)
