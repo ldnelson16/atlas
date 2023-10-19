e858cf9c8a7da51c177daf4662faa8eddba4e8f8
ldnelson16 <ldnelson16@gmail.com>
2023-10-18 20:46:55 -0400

    abstracting functionality from app.py

--- atlas-app/app.py
+++ atlas-app/app.py
@@ -1,51 +1,19 @@
 import tkinter as tk
 from semester_analysis.total_evaluation import total_evaluation_page
-from change_page import change_page
+from components.menu import menu
 import json
 
 with open("atlas-app/pages.json", 'r') as json_file:
   paths = json.load(json_file)
-
-def create_popup():
-    popup = tk.Toplevel(root)
-    popup.geometry('800x500')
-    popup.title("Semster Workload Analysis")
-    label = tk.Label(popup, text="This is a popup where I will put Semester Workload Analysis.")
-    label.pack()
-
-def on_menu_hover(event):
-    # Display the dropdown menu when the menu item is hovered
-    menu.post(event.x_root, event.y_root)
-
-def on_menu_leave(event):
-    # Hide the dropdown menu when the mouse leaves the menu item
-    menu.unpost()
 
 root = tk.Tk()
 root.title("UMICH Course Analysis")
 root.geometry('800x500')
 
 # Create a Menu widget for the top-level items
-menu = tk.Menu(root)
-
-# Create a Menu widget for the dropdown
-dropdown_menu1 = tk.Menu(menu, tearoff=0)
-
-# Add items to the dropdown menu
-dropdown_menu1.add_command(label="Total Evaluation",command=lambda:change_page("Total Evaluation",root))
-dropdown_menu1.add_command(label="Workload Evaluation",command=lambda:change_page("Workload Evaluation",root))
-
-# Add top-level items with dropdown menus
-menu.add_cascade(label="Semester Analysis", menu=dropdown_menu1)
-
-
-# Attach the menu to the root window
-root.config(menu=menu)
+menu(root)
 
 title_label = tk.Label(root, text="Welcome to UMICH Course Analysis", font=("Arial", 16))
 title_label.pack(pady=20)
-
-button = tk.Button(root, text="Semester Workload Analysis", command=create_popup)
-button.pack()
 
 root.mainloop()
