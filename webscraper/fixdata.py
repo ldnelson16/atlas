ac60ea0cb21acc6376ab50d6b75de8b11503f86e
ldnelson16 <ldnelson16@gmail.com>
2023-11-01 13:18:05 -0400

    grade/credits data stored in list

--- webscraper/fixdata.py
+++ webscraper/fixdata.py
@@ -5,19 +5,21 @@ with open("webscraper/data/atlasdictfinal1.json","r") as file:
 
 to_remove = []
 for key in results:
+  if results[key]["Median Grade"] == "N/A":
+    results[key]["Median Grade"] = None
+  elif results[key]["Median Grade"]!=None and "~" in results[key]["Median Grade"]:
+    results[key]["Median Grade"] = results[key]["Median Grade"].split("~")
+  elif results[key]["Median Grade"]!=None:
+    results[key]["Median Grade"] = [results[key]["Median Grade"],results[key]["Median Grade"]]
   if results[key]["Class Title"]==None:
-    print(key,"missing")
     to_remove+=[key]
   try: 
     x=float(results[key]["Credits"])
-    # print(key,[float(results[key]["Credits"]),float(results[key]["Credits"])])
     results[key]["Credits"] = [float(results[key]["Credits"]),float(results[key]["Credits"])]
   except:
     try:
-      # print(key,[float(x) for x in results[key]["Credits"].split('-')])
       results[key]["Credits"] = [float(x) for x in results[key]["Credits"].split('-')]
     except:
-      # print(key,results[key]["Credits"])
       pass
 
 for key in to_remove:
